13ebf6f8d4e59538147524efc96838ac89f98906
Darshan Subedi <darshansubedi.cst@rub.edu.bt>
2025-02-24 15:27:22 +0600

    fifth commit

--- practical0.py
+++ practical0.py
@@ -3,7 +3,7 @@ studentNumber = "02190108"
 
 Bank_accoount = "changkhaasdfasdfasdf"
 
-Bum_jarims_phone_number = 171234123413
+
 
 print("My name is " + studentName + " and my student number is " + studentNumber)
 
